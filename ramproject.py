7864bb3b55f4211b5c1719daa18d97ed54d2a1ac
hardikpython <pk646155@gmail.com>
2025-02-14 16:43:40 +0530

    stringprogram

---
+++ ramproject.py
@@ -0,0 +1,8 @@
+# Input the congratulation message in lowercase
+message = input("Enter your congratulation message in lowercase: ")
+
+# Convert to uppercase
+congratulated_message = message.upper()
+
+# Display the congratulated message
+print("Congratulations message in uppercase:", congratulated_message)
